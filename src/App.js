e27b7f81572721f57c15e4dbf1df24cbc9737574
Michael J Maggs <seattleslew@runawayserver.com>
2023-06-05 16:48:49 -0700

    passing state to components vs searchChange fn

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const state = {
-  robots: robots,
-  searchfield: ''
-};
 
 export class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    };
+  }
+
+  onSearchChange( event ) {
+    console.log( event );
+  }
   render() {
     return (
       <div className='tc' >
         <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={ robots } />
+        <SearchBox onSearchChange={ this.onSearchChange } />
+        <CardList robots={ this.state.robots } />
       </div >
     );
   };
