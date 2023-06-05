637c911044c7606cb11f1c755ef5a4638c3aaef5
Michael J Maggs <seattleslew@runawayserver.com>
2023-06-05 06:59:55 -0700

    layering in state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
-import SearchBox from 'SearchBox';
+import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
-  return (
-    <div>
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={ robots } />
-    </div >
-  );
+const state = {
+  robots: robots,
+  searchfield: ''
 };
+
+export class App extends Component {
+  render() {
+    return (
+      <div className='tc' >
+        <h1>RoboFriends</h1>
+        <SearchBox />
+        <CardList robots={ robots } />
+      </div >
+    );
+  };
+}
 export default App;
