0a74861191eb7b0563ed85ed6c463d502bffcf5d
Michael J Maggs <seattleslew@runawayserver.com>
2023-06-04 19:02:43 -0700

    additional changes to App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import CardList from './CardList';
+import SearchBox from 'SearchBox';
 import { robots } from './robots';
 
 const App = () => {
   return (
-    <CardList robots={ robots } />
+    <div>
+      <h1>RoboFriends</h1>
+      <SearchBox />
+      <CardList robots={ robots } />
+    </div >
   );
 };
 export default App;
