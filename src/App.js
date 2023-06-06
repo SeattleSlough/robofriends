45f44440dd219f1e76a2085b7ce3dca2a3db1653
Michael J Maggs <seattleslew@runawayserver.com>
2023-06-05 17:39:54 -0700

    final state edits

--- src/App.js
+++ src/App.js
@@ -13,15 +13,21 @@ export class App extends Component {
     };
   }
 
-  onSearchChange( event ) {
-    console.log( event );
-  }
+  onSearchChange = ( event ) => {
+    this.setState( { searchfield: event.target.value } );
+  };
+
   render() {
+    const filteredRobots = this.state.robots.filter( robot => {
+      return robot.name.toLowerCase().includes( this.state.searchfield.toLowerCase() );
+    }
+    );
+
     return (
       <div className='tc' >
         <h1>RoboFriends</h1>
         <SearchBox searchChange={ this.onSearchChange } />
-        <CardList robots={ this.state.robots } />
+        <CardList robots={ filteredRobots } />
       </div >
     );
   };
