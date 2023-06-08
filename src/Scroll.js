ce0cee7b79a6afcd8e983aa6c90cae9c63c7ab38
Michael J Maggs <seattleslew@runawayserver.com>
2023-06-08 13:54:30 -0700

    added css code to Scroll wrapper elements

--- src/Scroll.js
+++ src/Scroll.js
@@ -4,13 +4,12 @@
 
 import React from 'react';
 
-const Scroll = ( props ) => { // contains 
+const Scroll = ( props ) => {
    return (
-      <div>
+      <div style={ { overflowY: 'scroll', border: '1px solid black', height: '500px' } }>
          { props.children }
       </div>
    );
-
 };
 
 export default Scroll;
